9600bfa224f488d7e96d0e21c4fe3cc3b8697b29
someshwar reddy <somesh.satish@gmail.com>
2023-02-20 21:29:26 +0530

    someshwar profile

---
+++ src/app/about/about.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AboutComponent } from './about/about.component';
+import { AboutRoutingModule } from './about-routing.module';
+
+
+
+@NgModule({
+
+  imports: [
+    CommonModule,
+    AboutRoutingModule
+  ],
+  declarations: [
+    AboutComponent
+  ],
+})
+export class AboutModule { }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {
+    path: 'about',
+    loadChildren: () => import('./about/about.module').then(m => m.AboutModule)
+  },
+  {
+    path: 'projects',
+    loadChildren: () => import('./projects/projects.module').then(m => m.ProjectsModule)
+  },
+  {
+    path: 'blog',
+    loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule)
+  },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
