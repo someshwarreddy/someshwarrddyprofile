0f6127290e247898fab6b79a8445d0786006283c
someshwar reddy <somesh.satish@gmail.com>
2023-07-08 11:36:17 +0530

    newcomponents

--- src/app/blog/blog-routing.module.ts
+++ src/app/blog/blog-routing.module.ts
@@ -1,12 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BlogComponent } from './blog/blog.component';
+import { HtmlComponent } from './html/html.component';
+import { CssComponent } from './css/css.component';
+import { AngularComponent } from './angular/angular.component';
+import { JavascriptComponent } from './javascript/javascript.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: BlogComponent
+  },
+  {
+    path: 'html',
+    component: HtmlComponent
+  },
+  {
+    path: 'css',
+    component: CssComponent
+  },
+  {
+    path: 'angular',
+    component: AngularComponent
+  },
+  {
+    path:'javascript',
+    component:JavascriptComponent
+  }
 
-const routes: Routes = [{
-  path: '',
-  component: BlogComponent
-}];
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -3,11 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { BlogRoutingModule } from './blog-routing.module';
 import { BlogComponent } from './blog/blog.component';
+import { HtmlComponent } from './html/html.component';
+import { CssComponent } from './css/css.component';
+import { AngularComponent } from './angular/angular.component';
+import { JavascriptComponent } from './javascript/javascript.component';
 
 
 @NgModule({
   declarations: [
-    BlogComponent
+    BlogComponent,
+    HtmlComponent,
+    CssComponent,
+    AngularComponent,
+    JavascriptComponent
   ],
   imports: [
     CommonModule,
