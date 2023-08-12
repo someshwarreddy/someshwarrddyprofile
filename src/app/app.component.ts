7bc78a095d8cc65607cbc3492b7bacbc7ba8d301
someshwar reddy <somesh.satish@gmail.com>
2023-08-12 22:40:42 +0530

    new changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { Renderer2 } from '@angular/core'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  
+  constructor(private renderer: Renderer2) {}
+   url='/src/assets/someshwarresume.docx'
+   downloadFile() {
+      const link = this.renderer.createElement('a');
+      link.setAttribute('target', '_blank');
+      link.setAttribute('href', this.url);
+      link.setAttribute('download', `someshwarresume.docx`);
+      link.click();
+      link.remove();
+   }
 }
