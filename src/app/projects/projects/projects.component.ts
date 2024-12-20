fbba5ff42f181d68fadaa636a0e5520566930750
someshwar reddy <somesh.satish@gmail.com>
2024-12-20 10:20:48 +0530

    merged details in one page

--- src/app/projects/projects/projects.component.ts
+++ src/app/projects/projects/projects.component.ts
@@ -12,25 +12,39 @@ export class ProjectsComponent {
     {
       id: 1,
       title: 'Countries',
-      image: './assets/images/p1.png'
+      summary: 'The project scope is to show all countries and each country details on an individual webpage and provide a search result to users searching by country name or region.',
+      image: './assets/images/p1.png',
+      pl: '  Angualr , Css , Html , Ngrx',
+      URL: 'https://countriesall.netlify.app/countries'
+
     },
     /*...*/
     {
       id: 2,
       title: 'Justcart',
-      image: './assets/images/p2.png'
+      summary: 'The project scope is to show all products and each product details on an individual webpage and add each product to a cart in cart increments decrement the quantity of each product and provide a search result to users searching by product title or categories. Note: the project was developed with a fake API store.',
+      image: './assets/images/p2.png',
+      pl: ' Angualr , Css , Html , Ngrx',
+      URL: 'https://justkart.netlify.app/'
+
     },
-     /*...*/
-     {
+    /*...*/
+    {
       id: 3,
       title: 'uicontainer',
+      summary: 'The scope of the project is angular devlope ui components like tabs , accordian .',
       image: './assets/images/p4.png',
+      pl: '  Angualr , Css , Html , Ngrx',
+      URL: 'https://uicontainer.netlify.app/'
     },
     /*...*/
     {
       id: 4,
       title: 'Someshwar reddy ',
-      image: './assets/images/p-3.png'
+      summary: 'The scope of the project is my portfolio.',
+      image: './assets/images/p-3.png',
+      pl: '  Angualr , Css , Html , Ngrx',
+      URL: 'https://someshwarreddy.netlify.app/'
     }
   ]
 }
